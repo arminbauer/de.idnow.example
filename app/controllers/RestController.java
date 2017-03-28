f830d6071e8c969c94ebc2fa35cef90d23094d61
Catalin <catalin.purice@astraia.com>
2017-03-28 10:34:29 +0200

    My changes

--- app/controllers/RestController.java
+++ app/controllers/RestController.java
@@ -1,39 +1,49 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.avaje.ebean.Model;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import models.Company;
+import models.Identification;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import util.Utility;
 
-import play.libs.Json;
-import play.mvc.*;
+import java.util.Collections;
+import java.util.List;
+
+import static play.libs.Json.toJson;
 
 public class RestController extends Controller {
 
-    public Result startIdentification() {
-    	//Get the parsed JSON data
-    	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the identification
-    	
-        return ok();
+    public Result startIdentification() throws JsonProcessingException {
+        Identification identification = Form.form(Identification.class).bindFromRequest().get();
+        identification.save();
+        return redirect(routes.Application.index());
     }
 
     public Result addCompany() {
-    	//Get the parsed JSON data
-    	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the company
-    	
-        return ok();
+        Company company = Form.form(Company.class).bindFromRequest().get();
+        company.save();
+        return redirect(routes.Application.index());
+    }
+
+    public Result getCompanies() {
+        List<Company> companies = new Model.Finder(Company.class).all();
+        return ok(toJson(companies));
     }
 
     public Result identifications() {
-    	JsonNode identifications = Json.newArray();
-    	
-    	//Get the current identification
-    	//Compute correct order
-    	//Create new identification JSON with JsonNode identification = Json.newObject();
-    	//Add identification to identifications list 
-    	
-        return ok(identifications);
+        List<Identification> identifications = new Model.Finder(Identification.class).all();
+        return ok(toJson(identifications));
+    }
+
+    public Result pendingIdentifications(){
+        List<Identification> identifications = new Model.Finder(Identification.class).all();
+        List<Company> companies = new Model.Finder(Company.class).all();
+        Utility.setCompaniesForeignKeys(identifications, companies);
+        Collections.sort(identifications);
+        return ok(toJson(identifications));
     }
 
 }
