d8645111b52b3dde33d766f85fc8190c5c4f96e7
Chanchal1985 <chanchalnerkar@gmail.com>
2017-04-23 21:32:41 +0200

    Controller logic uploaded

--- app/controllers/RestController.java
+++ app/controllers/RestController.java
@@ -1,39 +1,45 @@
 package controllers;
 
+import java.util.List;
+import java.util.Set;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
+import dto.Company;
+import dto.Identification;
 import play.libs.Json;
+import play.libs.*;
 import play.mvc.*;
+import service.IdentService;
 
 public class RestController extends Controller {
 
-    public Result startIdentification() {
-    	//Get the parsed JSON data
-    	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the identification
-    	
-        return ok();
-    }
+	IdentService identService = new IdentService();
 
-    public Result addCompany() {
-    	//Get the parsed JSON data
-    	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the company
-    	
-        return ok();
-    }
+	public Result startIdentification() {
+		//Get the parsed JSON data
+		JsonNode json = request().body().asJson();
 
-    public Result identifications() {
-    	JsonNode identifications = Json.newArray();
-    	
-    	//Get the current identification
-    	//Compute correct order
-    	//Create new identification JSON with JsonNode identification = Json.newObject();
-    	//Add identification to identifications list 
-    	
-        return ok(identifications);
-    }
+		//Do something with the identification
+		Identification identification = Json.fromJson( json , Identification.class);
+		try{
+			identService.addIdentification(identification);
+		}catch(Exception ex){
+			return internalServerError(ex.getMessage());
+		}
+		return ok();
+	}
+
+	public Result addCompany() {
+		//Get the parsed JSON data
+		JsonNode json = request().body().asJson();
+		identService.addCompany(Json.fromJson( json , Company.class));
+		return ok();
+	}
+
+	public Result identifications() {
+		JsonNode identifications = Json.newArray();
+		return ok(Json.toJson(identService.getIdentifications()));
+	}
 
 }
