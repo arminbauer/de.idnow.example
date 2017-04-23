f69d32c1fb219b8d4cc3398b3e2ddd47d9b04869
Chanchal1985 <chanchalnerkar@gmail.com>
2017-04-23 21:33:52 +0200

    Junit testcases added

--- test/RestControllerTest.java
+++ test/RestControllerTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.OK;
+import static play.mvc.Http.Status.INTERNAL_SERVER_ERROR;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
@@ -11,6 +12,8 @@ import play.libs.Json;
 import play.libs.ws.WS;
 
 import com.fasterxml.jackson.databind.JsonNode;
+
+import dto.Identification;
 
 /**
 *
@@ -38,14 +41,32 @@ public class RestControllerTest {
 		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
 			@Override
 			public void run() {
-				JsonNode company = Json.parse("{\"id\": 1, \"name\": \"Test Bank\", \"sla_time\": 60, \"sla_percentage\": 0.9, \"current_sla_percentage\": 0.95}");
+				JsonNode company = Json.parse("{\"id\": 1, \"name\": \"Test Bank\", \"slaTime\": 60, \"slaPercentage\": 0.9, \"currSlaPercentage\": 0.95}");
 				assertEquals(WS.url("http://localhost:3333/api/v1/addCompany").post(company).get(10000).getStatus(), OK);
 				
-				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waiting_time\": 10, \"companyid\": 1}");
+				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waitingTime\": 10, \"companyId\": 1}");
 				assertEquals(WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus(), OK);
 			}
 		});
 
 	}
-
+	
+	/**
+	 * Test method to test  exceptiom scenario when company for identification 
+	 * job does not exists
+	 */
+	@Test
+	public void postIdentificationInValidCompany() {
+		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
+			@Override
+			public void run() {
+				JsonNode company = Json.parse("{\"id\": 1, \"name\": \"Test Bank\", \"slaTime\": 60, \"slaPercentage\": 0.9, \"currSlaPercentage\": 0.95}");
+				assertEquals(WS.url("http://localhost:3333/api/v1/addCompany").post(company).get(10000).getStatus(), OK);
+				
+				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waitingTime\": 10, \"companyId\": 10}");
+				assertEquals(WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus(), INTERNAL_SERVER_ERROR);
+			}
+		});
+		
+	}
 }
