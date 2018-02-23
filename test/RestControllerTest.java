b2a5f78c67fbdae293b5d8255123c35b2f089d6e
andrii.karmeliuk <karmeliuk@gmail.com>
2018-02-23 12:42:23 +0100

    format, use uuids, fix param order in assertEquals

--- test/RestControllerTest.java
+++ test/RestControllerTest.java
@@ -1,51 +1,44 @@
-import static org.junit.Assert.assertEquals;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.inMemoryDatabase;
-import static play.test.Helpers.running;
-import static play.test.Helpers.testServer;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
-
 import play.libs.Json;
 import play.libs.ws.WS;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
 
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ * Simple (JUnit) tests that can call all parts of a play app.
+ * If you are interested in mocking a whole application, see the wiki for more details.
+ */
 public class RestControllerTest {
 
-	JsonNode identifications;
+    JsonNode identifications;
 
-	@Test
-	public void getIdentifications() {
-		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
-			@Override
-			public void run() {
-				assertEquals(WS.url("http://localhost:3333/api/v1/identifications").get().get(10000).getStatus(), OK);
-			}
-		});
+    @Test
+    public void getIdentifications() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
+            @Override
+            public void run() {
+                assertEquals(OK, WS.url("http://localhost:3333/api/v1/identifications").get().get(10000).getStatus());
+            }
+        });
 
-	}
+    }
 
-	@Test
-	public void postIdentification() {
-		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
-			@Override
-			public void run() {
-				JsonNode company = Json.parse("{\"id\": 1, \"name\": \"Test Bank\", \"sla_time\": 60, \"sla_percentage\": 0.9, \"current_sla_percentage\": 0.95}");
-				assertEquals(WS.url("http://localhost:3333/api/v1/addCompany").post(company).get(10000).getStatus(), OK);
-				
-				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waiting_time\": 10, \"companyid\": 1}");
-				assertEquals(WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus(), OK);
-			}
-		});
+    @Test
+    public void postIdentification() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
+            @Override
+            public void run() {
+                JsonNode company = Json.parse("{\"id\": \"123e4567-e89b-42d3-a456-556642440000\", \"name\": \"Test Bank\", \"sla_time\": 60, \"sla_percentage\": 0.9, \"current_sla_percentage\": 0.95}");
+                assertEquals(OK, WS.url("http://localhost:3333/api/v1/addCompany").post(company).get(10000).getStatus());
 
-	}
+                JsonNode identification = Json.parse("{\"id\": \"123e4568-e89b-42d3-a456-556642440000\", \"name\": \"Peter Huber\", \"time\": 1435667215, \"waiting_time\": 10, \"companyId\": \"123e4567-e89b-42d3-a456-556642440000\"}");
+                assertEquals(OK, WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus());
+            }
+        });
+
+    }
 
 }
