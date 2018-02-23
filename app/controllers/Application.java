f3a00b38d48acde7a0a07b0f2d3b5fde872bbdb1
andrii.karmeliuk <karmeliuk@gmail.com>
2018-02-23 16:33:35 +0100

    add ident_form view draft - not able to submit with application/json contentType

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,27 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.Identification;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.ident_form;
+import views.html.index;
 
-import views.html.*;
+import java.util.UUID;
 
 public class Application extends Controller {
 
     public Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public Result ident_form() {
+        Form<Identification> form = Form.form(Identification.class);
+
+        Identification ident = new Identification(UUID.randomUUID(), "Name", System.currentTimeMillis() / 1000, 45L, UUID.randomUUID());
+        form.fill(ident);
+
+        return ok(ident_form.render(form));
     }
 
 }
