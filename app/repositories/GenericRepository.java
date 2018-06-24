6e7a020e03d392e3f0e6d6f0fa0e45b9862eddce
Hebert Coelho <uaihebert@gmail.com>
2018-06-24 23:51:52 +0200

    fixup! create a generic repository to give a basic support for all models

--- app/repositories/GenericRepository.java
+++ app/repositories/GenericRepository.java
@@ -2,21 +2,19 @@ package repositories;
 
 import play.db.jpa.JPA;
 import play.db.jpa.JPAApi;
-import play.db.jpa.Transactional;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 public class GenericRepository<T> {
 
-    private final JPAApi jpaApi;
+    final JPAApi jpaApi;
 
     @Inject
     public GenericRepository(JPAApi jpaApi) {
         this.jpaApi = jpaApi;
     }
 
-    @Transactional
     public void save(T entity) {
         jpaApi.withTransaction(() -> {
             EntityManager em = JPA.em();
