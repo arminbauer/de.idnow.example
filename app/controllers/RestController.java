0da841a871b163004857befbcf6a37552741c834
Hebert Coelho <uaihebert@gmail.com>
2018-06-24 14:18:34 +0200

    adapt controllers to return persisted models, refactor on routes
    I changed the URLs to follow more the REST pattern

--- app/controllers/RestController.java
+++ app/controllers/RestController.java
@@ -2,27 +2,40 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import models.Company;
+import models.Identification;
 import play.libs.Json;
 import play.mvc.*;
+import repositories.GenericRepository;
+import services.IdentificationService;
+
+import javax.inject.Inject;
 
 public class RestController extends Controller {
+
+    @Inject
+    private GenericRepository<Company> companyRepository;
+
+    @Inject
+    private IdentificationService identificationService;
 
     public Result startIdentification() {
     	//Get the parsed JSON data
     	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the identification
-    	
-        return ok();
+        Identification identification = Json.fromJson(json, Identification.class);
+
+        identificationService.save(identification, json.get("companyid").asInt());
+
+        return created(Json.toJson(identification));
     }
 
     public Result addCompany() {
-    	//Get the parsed JSON data
     	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the company
-    	
-        return ok();
+        Company company = Json.fromJson(json, Company.class);
+
+        companyRepository.save(company);
+
+        return created(Json.toJson(company));
     }
 
     public Result identifications() {
