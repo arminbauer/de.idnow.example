58625941095dc27f985c5ad85e092ec7caa07e95
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-08-04 19:39:54 +0200

    A little bit of cleanup:  - Cleanup Tests  - Cleanup Application.java  - Remove RestController  - Remove RestControllerTest

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,10 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
+@SuppressWarnings("unused")
 public class Application extends Controller {
   public Result index() {
     return ok(index.render("Your new application is ready."));
