df90ec45dfb3939d114946b4432c1d2827d715d5
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-08-04 21:21:56 +0200

    Controllers implementation:  - Remove usage of deprecated helper method form applicationTest

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -4,14 +4,13 @@ import play.twirl.api.Content;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.contentType;
 
 
 public class ApplicationTest {
   @Test
   public void renderTemplate() {
     final Content html = views.html.index.render("Your new application is ready.");
-    assertEquals("text/html", contentType(html));
+    assertEquals("text/html", html.contentType());
     assertTrue(contentAsString(html).contains("Your new application is ready."));
   }
 }
