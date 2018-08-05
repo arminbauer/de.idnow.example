0ab0db6dc7a18c00e69b8acbde788355a36e1e94
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-08-05 16:35:18 +0200

    UI implementation:  - Add pretty dumb UI

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,11 +2,25 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import repositories.CompanyRepository;
+import repositories.IdentificationRepository;
+import views.html.main;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 @SuppressWarnings("unused")
 public class Application extends Controller {
+  private final IdentificationRepository identificationRepository;
+  private final CompanyRepository companyRepository;
+
+  @Inject
+  public Application(@Nonnull final IdentificationRepository identificationRepository, @Nonnull final CompanyRepository companyRepository) {
+    this.identificationRepository = identificationRepository;
+    this.companyRepository = companyRepository;
+  }
+
   public Result index() {
-    return ok(index.render("Your new application is ready."));
+    return ok(main.render(identificationRepository.allPending(), companyRepository.all()));
   }
 }
