637eaeba211c413f6a804fd6a255ef404770e7df
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-08-05 17:38:30 +0200

    UI implementation:  - Add test for main page

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,16 +1,31 @@
+import models.Company;
+import models.Identification;
 import org.junit.Test;
+import play.data.Form;
+import play.db.Database;
+import play.db.evolutions.Evolutions;
+import play.test.FakeApplication;
+import play.test.Helpers;
 import play.twirl.api.Content;
+
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.*;
 
 
 public class ApplicationTest {
   @Test
   public void renderTemplate() {
-    final Content html = views.html.index.render("Your new application is ready.");
-    assertEquals("text/html", html.contentType());
-    assertTrue(contentAsString(html).contains("Your new application is ready."));
+    final FakeApplication app = Helpers.fakeApplication();
+    final Database db = app.injector().instanceOf(Database.class);
+    running(testServer(3333, app), () -> {
+      final Content html = views.html.main.render(Collections.emptyList(), Collections.emptyList(), Form.form(Identification.class), Form.form(Company.class));
+      assertEquals("text/html", html.contentType());
+      assertTrue(contentAsString(html).contains("Identifications"));
+      assertTrue(contentAsString(html).contains("Companies"));
+      Evolutions.cleanupEvolutions(db);
+    });
   }
 }
