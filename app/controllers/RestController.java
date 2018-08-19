c65e095671fc53af7c401e0eaba35eb012f6dbf0
talal830 <talal830@gmail.com>
2018-08-19 17:02:52 +0200

    Working version test against example scenarios

--- app/controllers/RestController.java
+++ app/controllers/RestController.java
@@ -1,39 +1,51 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import models.Company;
+import models.Identification;
 import play.libs.Json;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.IdentificationService;
+
+import javax.inject.Inject;
+import java.util.SortedSet;
 
 public class RestController extends Controller {
 
+    @Inject
+    private IdentificationService service;
+
     public Result startIdentification() {
-    	//Get the parsed JSON data
-    	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the identification
-    	
+        //Get the parsed JSON data
+        JsonNode json = request().body().asJson();
+
+        Identification identification = Identification.fromJson(json);
+        service.addIdentification(identification);
+
         return ok();
     }
 
     public Result addCompany() {
-    	//Get the parsed JSON data
-    	JsonNode json = request().body().asJson();
-    	
-    	//Do something with the company
-    	
+        //Get the parsed JSON data
+        JsonNode json = request().body().asJson();
+
+        Company company = Company.fromJson(json);
+        service.addCompany(company);
+
         return ok();
     }
 
     public Result identifications() {
-    	JsonNode identifications = Json.newArray();
-    	
-    	//Get the current identification
-    	//Compute correct order
-    	//Create new identification JSON with JsonNode identification = Json.newObject();
-    	//Add identification to identifications list 
-    	
-        return ok(identifications);
+        // Get prioritized identifications list
+        SortedSet<Identification> priorityList = service.priorityList();
+
+        // Create array of JsonNode
+        JsonNode jsonIdents = Json.newArray();
+        priorityList.forEach(x -> ((ArrayNode) jsonIdents).add(x.toJson()));
+
+        return ok(jsonIdents);
     }
 
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,15 +1,21 @@
-import org.junit.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import models.Company;
+import models.Identification;
+import org.junit.Test;
+import play.libs.F.Callback;
+import play.libs.ws.WS;
+import play.libs.ws.WSResponse;
+import play.test.TestBrowser;
 
-import play.mvc.*;
-import play.test.*;
-import play.libs.F.*;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.*;
-import static org.junit.Assert.*;
-
-import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
+
+    private static final String ADD_COMPANY_URL = "http://localhost:3333/api/v1/addCompany";
+    private static final String START_IDENTIFICATION_URL = "http://localhost:3333/api/v1/startIdentification";
+    private static final String IDENTIFICATIONS_URL = "http://localhost:3333/api/v1/identifications";
 
     /**
      * add your integration test here
@@ -22,6 +28,204 @@ public class IntegrationTest {
                 browser.goTo("http://localhost:3333");
                 assertTrue(browser.pageSource().contains("Your new application is ready."));
             }
+        });
+    }
+
+    @Test
+    public void testScenario1() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), () -> {
+            Company company = new Company.Builder()
+                    .id(1)
+                    .name("Company-1")
+                    .slaTime(60)
+                    .slaPercentage(0.9)
+                    .currentSLAPercentage(0.95)
+                    .get();
+
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company.toJson()).get(10000).getStatus(), OK);
+
+            Identification ident1 = new Identification.Builder()
+                    .id(1)
+                    .name("A")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(30)
+                    .companyId(company.getId())
+                    .get();
+
+
+            Identification ident2 = new Identification.Builder()
+                    .id(2)
+                    .name("B")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(45)
+                    .companyId(company.getId())
+                    .get();
+
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident2.toJson()).get(10000).getStatus(), OK);
+
+            WSResponse wsResponse = WS.url(IDENTIFICATIONS_URL).get().get(10000);
+            assertEquals(wsResponse.getStatus(), OK);
+
+            JsonNode jsonNode = wsResponse.asJson();
+            assertEquals(Identification.fromJson(jsonNode.get(0)).getId(), ident2.getId());
+            assertEquals(Identification.fromJson(jsonNode.get(1)).getId(), ident1.getId());
+        });
+    }
+
+    @Test
+    public void testScenario2() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), () -> {
+            Company company1 = new Company.Builder()
+                    .id(1)
+                    .name("Company-1")
+                    .slaTime(60)
+                    .slaPercentage(0.9)
+                    .currentSLAPercentage(0.95)
+                    .get();
+
+            Company company2 = new Company.Builder()
+                    .id(2)
+                    .name("Company-2")
+                    .slaTime(60)
+                    .slaPercentage(0.9)
+                    .currentSLAPercentage(0.90)
+                    .get();
+
+
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company2.toJson()).get(10000).getStatus(), OK);
+
+            Identification ident1 = new Identification.Builder()
+                    .id(1)
+                    .name("A")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(30)
+                    .companyId(company1.getId())
+                    .get();
+
+
+            Identification ident2 = new Identification.Builder()
+                    .id(2)
+                    .name("B")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(30)
+                    .companyId(company2.getId())
+                    .get();
+
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident2.toJson()).get(10000).getStatus(), OK);
+
+            WSResponse wsResponse = WS.url(IDENTIFICATIONS_URL).get().get(10000);
+            assertEquals(wsResponse.getStatus(), OK);
+
+            JsonNode jsonNode = wsResponse.asJson();
+            assertEquals(Identification.fromJson(jsonNode.get(0)).getId(), ident2.getId());
+            assertEquals(Identification.fromJson(jsonNode.get(1)).getId(), ident1.getId());
+        });
+    }
+
+    @Test
+    public void testScenario3() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), () -> {
+            Company company1 = new Company.Builder()
+                    .id(1)
+                    .name("Company-1")
+                    .slaTime(60)
+                    .slaPercentage(0.9)
+                    .currentSLAPercentage(0.95)
+                    .get();
+
+            Company company2 = new Company.Builder()
+                    .id(2)
+                    .name("Company-2")
+                    .slaTime(120)
+                    .slaPercentage(0.8)
+                    .currentSLAPercentage(0.95)
+                    .get();
+
+
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company2.toJson()).get(10000).getStatus(), OK);
+
+            Identification ident1 = new Identification.Builder()
+                    .id(1)
+                    .name("A")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(30)
+                    .companyId(company1.getId())
+                    .get();
+
+
+            Identification ident2 = new Identification.Builder()
+                    .id(2)
+                    .name("B")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(30)
+                    .companyId(company2.getId())
+                    .get();
+
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident2.toJson()).get(10000).getStatus(), OK);
+
+            WSResponse wsResponse = WS.url(IDENTIFICATIONS_URL).get().get(10000);
+            assertEquals(wsResponse.getStatus(), OK);
+
+            JsonNode jsonNode = wsResponse.asJson();
+            assertEquals(Identification.fromJson(jsonNode.get(0)).getId(), ident1.getId());
+            assertEquals(Identification.fromJson(jsonNode.get(1)).getId(), ident2.getId());
+        });
+    }
+
+    @Test
+    public void testScenario4() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), () -> {
+            Company company1 = new Company.Builder()
+                    .id(1)
+                    .name("Company-1")
+                    .slaTime(60)
+                    .slaPercentage(0.9)
+                    .currentSLAPercentage(0.95)
+                    .get();
+
+            Company company2 = new Company.Builder()
+                    .id(2)
+                    .name("Company-2")
+                    .slaTime(120)
+                    .slaPercentage(0.8)
+                    .currentSLAPercentage(0.80)
+                    .get();
+
+
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(ADD_COMPANY_URL).post(company2.toJson()).get(10000).getStatus(), OK);
+
+            Identification ident1 = new Identification.Builder()
+                    .id(1)
+                    .name("A")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(45)
+                    .companyId(company1.getId())
+                    .get();
+
+
+            Identification ident2 = new Identification.Builder()
+                    .id(2)
+                    .name("B")
+                    .time(System.currentTimeMillis())
+                    .waitingTime(30)
+                    .companyId(company2.getId())
+                    .get();
+
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident1.toJson()).get(10000).getStatus(), OK);
+            assertEquals(WS.url(START_IDENTIFICATION_URL).post(ident2.toJson()).get(10000).getStatus(), OK);
+
+            WSResponse wsResponse = WS.url(IDENTIFICATIONS_URL).get().get(10000);
+            assertEquals(wsResponse.getStatus(), OK);
+
+            JsonNode jsonNode = wsResponse.asJson();
+            assertEquals(Identification.fromJson(jsonNode.get(0)).getId(), ident1.getId());
+            assertEquals(Identification.fromJson(jsonNode.get(1)).getId(), ident2.getId());
         });
     }
 
