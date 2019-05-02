f7a2a6d16cf200c3ac099bf45b31fb96211c9ad2
Quan Nguyen <>
2019-05-02 10:39:37 +0700

    - Update test for Input checking to improve code coverage.

--- test/RestControllerTest.java
+++ test/RestControllerTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
-import static play.mvc.Http.Status.BAD_REQUEST;
-import static play.mvc.Http.Status.OK;
+import static play.mvc.Http.Status.*;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
@@ -36,7 +35,7 @@ public class RestControllerTest {
 	}
 
 	@Test
-	public void postIdentification() {
+	public void postIdentification_OK() {
 		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
 			@Override
 			public void run() {
@@ -45,6 +44,33 @@ public class RestControllerTest {
 				
 				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waiting_time\": 10, \"companyid\": 1}");
 				assertEquals(WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus(), OK);
+			}
+		});
+
+	}
+
+	@Test
+	public void postIdentification_NoCompanyError() {
+		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
+			@Override
+			public void run() {
+				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waiting_time\": -10, \"companyid\": 1}");
+				assertEquals(WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus(), INTERNAL_SERVER_ERROR);
+			}
+		});
+
+	}
+
+	@Test
+	public void postIdentification_Error() {
+		running(testServer(3333, fakeApplication(inMemoryDatabase())), new Runnable() {
+			@Override
+			public void run() {
+				JsonNode company = Json.parse("{\"id\": 1, \"name\": \"Test Bank\", \"sla_time\": 0.0, \"sla_percentage\": 0.0, \"current_sla_percentage\": 0.95}");
+				assertEquals(WS.url("http://localhost:3333/api/v1/addCompany").post(company).get(10000).getStatus(), INTERNAL_SERVER_ERROR);
+
+				JsonNode identification = Json.parse("{\"id\": 1, \"name\": \"Peter Huber\", \"time\": 1435667215, \"waiting_time\": -10, \"companyid\": 1}");
+				assertEquals(WS.url("http://localhost:3333/api/v1/startIdentification").post(identification).get(10000).getStatus(), INTERNAL_SERVER_ERROR);
 			}
 		});
 
